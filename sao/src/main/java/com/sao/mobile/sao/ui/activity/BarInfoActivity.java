cda902d0ab29cfd5ac61a506df80bc4cc24bf8ba
Axel Vaillant <axelvaillantiris@gmail.com>
2017-03-07 17:22:37 +0100

    Vue information du bar, ajout de google maps

--- sao/src/main/java/com/sao/mobile/sao/ui/activity/BarInfoActivity.java
+++ sao/src/main/java/com/sao/mobile/sao/ui/activity/BarInfoActivity.java
@@ -2,8 +2,17 @@ package com.sao.mobile.sao.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.sao.mobile.sao.R;
 import com.sao.mobile.saolib.entities.Bar;
@@ -14,8 +23,11 @@ public class BarInfoActivity extends BaseActivity {
     private static final String TAG = BarInfoActivity.class.getSimpleName();
     private ImageView mBarThumbnail;
     private TextView mBarNom;
+    private TextView mbarDetails;
     private TextView mBarAdress;
     private TextView mBarTime;
+
+    static final LatLng PARIS = new LatLng(48.858093, 2.294694);
 
     public static final String BAR_EXTRA = "barExtra";
     private Bar mBar;
@@ -24,26 +36,41 @@ public class BarInfoActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_info);
-
         mBar = (Bar) getIntent().getSerializableExtra(BAR_EXTRA);
 
         setupHeader();
+        setupFooter();
+        setupMap();
     }
 
     private void setupHeader() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setupToolbar(toolbar);
-        getSupportActionBar().setTitle(getString(R.string.bar_info_title));
+        getSupportActionBar().setTitle(mBar.getName());
 
         mBarThumbnail = (ImageView) findViewById(R.id.barThumbnail);
         Picasso.with(mContext).load(mBar.getThumbnail()).fit().centerCrop().into(mBarThumbnail);
+    }
 
-        mBarNom = (TextView) findViewById(R.id.barName);
-        mBarNom.setText(mBar.getName());
+    private void setupFooter() {
+        mbarDetails = (TextView) findViewById(R.id.barDetails);
+        mbarDetails.setText(mBar.getDescription());
         mBarTime = (TextView) findViewById(R.id.barTime);
-        mBarTime.setText(mBar.getDescription());
+        mBarTime.setText(" Horaire : 9h / 18h");
         mBarAdress = (TextView) findViewById(R.id.barAdress);
         mBarAdress.setText(mBar.getAddress());
+    }
 
+    private void setupMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.addMarker(new MarkerOptions().title("Paris").position(PARIS));
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(PARIS, 15));
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+            }
+        });
     }
 }
